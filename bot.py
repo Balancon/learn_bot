d0d6b37b60d37be10109158cc01776b0746e385f
Vladimir Zadorozhnyy <balancon@mail.ru>
2024-05-21 11:14:24 +0300

    An update with a fix

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ import settings
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 
 # Proxy
-#PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
-#         'urllib3_proxy_kwargs': {'username': 'learn','password': 'python'}}
+#PROXY = {'proxy_url': '',
+#         '': {'username': '','password': ''}}
 
 def greet_user(update, context):
     print('Вызван /start')
@@ -21,7 +21,7 @@ def talk_to_me(update, context):
 
 # Функция, которая соединяется с платформой Telegram, "тело" нашего бота
 def main():
-#    mybot = Updater("7058214645:AAEMDBtCTR4nvXtYGmMVPPfbkbJA4GHgY6Y", use_context=True, request_kwargs=PROXY)
+
     mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
